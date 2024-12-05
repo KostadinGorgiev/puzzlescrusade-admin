a2be089a72a713cc51d6f8393d6d173af594cd6f
Kostadin <kostadingorgiev91@gmail.com>
2024-12-05 16:28:57 +0800

    update admin panel

--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -11,11 +11,13 @@ import {
   YAxis,
 } from "recharts";
 import { userLevel } from "../utils/service";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
   const [counts, setCounts] = useState([]);
   const [filterLevel, setFilterLevel] = useState([]);
+  const [totalCount, setTotalCount] = useState(1);
   const [page, setPage] = useState(1);
 
   const fetchUsersReport = async () => {
@@ -27,6 +29,7 @@ const Users = () => {
     );
     setUsers(response.data.users);
     setCounts(response.data.counts);
+    setTotalCount(response.data.total_count);
   };
 
   useEffect(() => {
@@ -172,6 +175,75 @@ const Users = () => {
                     ))}
                   </tbody>
                 </table>
+                <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+                  <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+                    <div>
+                      <p className="text-sm text-gray-700">
+                        Showing{" "}
+                        <span className="font-medium">
+                          {(page - 1) * 10 + 1}
+                        </span>{" "}
+                        to <span className="font-medium">{page * 10}</span> of{" "}
+                        <span className="font-medium">{totalCount}</span>{" "}
+                        results
+                      </p>
+                    </div>
+                    <div>
+                      <nav
+                        aria-label="Pagination"
+                        className="isolate inline-flex -space-x-px rounded-md shadow-sm"
+                      >
+                        <a
+                          href="#"
+                          className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                          onClick={() => {
+                            if (page > 1) {
+                              setPage(page - 1);
+                            }
+                          }}
+                        >
+                          <span className="sr-only">Previous</span>
+                          <ChevronLeftIcon
+                            aria-hidden="true"
+                            className="size-5"
+                          />
+                        </a>
+                        <div className="relative w-12 flex justify-center items-center py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 ">
+                          <input
+                            className="outline-none"
+                            style={{ width: `${page.toString().length / 2}em` }}
+                            value={page}
+                            min={0}
+                            max={Math.ceil(totalCount / 10)}
+                            onChange={(e) => {
+                              if (
+                                e.target.value > 0 &&
+                                e.target.value <= Math.ceil(totalCount / 10)
+                              ) {
+                                setPage(e.target.value);
+                              }
+                            }}
+                          />
+                        </div>
+                        <a
+                          href="#"
+                          className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                          onClick={() => {
+                            if (page < Math.ceil(totalCount / 10)) {
+                              setPage(page + 1);
+                            }
+                          }}
+                        >
+                          <span className="sr-only">Next</span>
+                          <ChevronRightIcon
+                            aria-hidden="true"
+                            className="size-5"
+                          />
+                        </a>
+                      </nav>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
